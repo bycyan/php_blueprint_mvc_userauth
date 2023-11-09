77e9c76264cf02608c997625fda3c03dd1a30cf1
Cyan Dalebout <cyan@wervelwindmedia.nl>
2023-11-09 12:55:27 +0100

    Kleine aanpassingen in styling

--- views/Base.php
+++ views/Base.php
@@ -35,24 +35,6 @@ abstract class Base
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@1,900&display=swap" rel="stylesheet">';
         //todo: toevoegen dynamisch css, page title
-        echo '<style>
-        body {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            gap: 50px;
-            height: 100vh;
-            margin: 0;
-            text-align: center;
-        }
-
-        nav ul{
-            display: flex;
-            gap: 30px;
-            list-style: none;
-        }
-        </style>';
     }
 
     protected function endHead()
--- views/BodyView.php
+++ views/BodyView.php
@@ -22,8 +22,7 @@ abstract class BodyView extends Base
     {
 
         echo '
-            <div class="outer-container navbar-outer center">
-                <nav class="inner-container navbar">
+                <nav>
                     <div class="menu-items">
                         <ul>
                             <li><a href="index.php?page=home">Home</a></li>
@@ -43,8 +42,7 @@ abstract class BodyView extends Base
                             <li><a href="index.php?page=register">Register</a></li>
                         </ul>
                     </div>
-                </nav>
-            </div>';
+                </nav>';
     }
 
     protected function beginMainContent()
